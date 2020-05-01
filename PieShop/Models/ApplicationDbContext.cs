7b7aa81341c3105315cc69ec7cf685306b3efd88
Husein Muftic <husein625@outlook.com>
2020-05-01 03:35:30 +0200

    Importing project

---
+++ PieShop/Models/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
+
---
+++ PieShop/Models/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _appDbContext;
+        public CategoryRepository(ApplicationDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        IEnumerable<Category> ICategoryRepository.AllCategories => _appDbContext.Categories;
+       
+    }
+}
